ff4091f8fa8c661917246d05f512268840544f88
whybigsz <ricardo.ferreira.2222@gmail.com>
2023-12-13 20:52:50 +0000

    hey

--- app/components/Herobot.jsx
+++ app/components/Herobot.jsx
@@ -1,6 +1,5 @@
+"use client"
 import { useRive } from '@rive-app/react-canvas';
-
-
 import React, { useEffect, useState } from 'react';
 
 export default function Herobot() {
--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { useRive,useStateMachineInput } from '@rive-app/react-canvas';
 import { useState, useEffect } from "react";
 
