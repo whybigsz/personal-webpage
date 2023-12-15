e6534cbde77052f07f1980a9f29804f243d25155
whybigsz <ricardo.ferreira.2222@gmail.com>
2023-12-15 15:39:57 +0000

    okay

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -7,6 +7,7 @@ HiRectangleGroup,
 HiChatBubbleBottomCenterText,
 HiEnvelope} from 'react-icons/hi2'
 
+import useDetectKeyboardOpen from "use-detect-keyboard-open";
 
 export const navData = [
   { name: 'Início', path: '/', icon: <HiHome  /> },
@@ -29,13 +30,14 @@ export const navData = [
 
     const router = useRouter();
     const pathname = router.pathname;
+    const isKeyboardOpen = useDetectKeyboardOpen();
 
     return (
       // <nav className='flex flex-col items-center xl:justify-center gap-y-4
       // fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-8 w-full
       // xl:w-16 xl:max-w-md xl:fixed
-      <nav   className='flex items-center justify-center fixed top-auto bottom-1/2 right-[2%] transform translate-y-1/2
-      w-16 max-w-md z-50 md:bottom-10 md:w-full md:left-1/2 md:-translate-x-1/2'>
+      <nav   className={`flex items-center justify-center fixed top-auto bottom-1/2 right-[2%] transform translate-y-1/2
+      w-16 max-w-md z-50 md:bottom-10 md:w-full md:left-1/2 md:-translate-x-1/2 ${isKeyboardOpen ? "xs:hidden" : ""}`}>
   <div className='flex flex-col md:flex-row items-center justify-between w-full  h-full gap-y-10 px-4 py-4 md:px-40
   xl:px-8  bg-black/30 rounded-full text-3xl xl:text-xl'>
           {navData.map((link,index) => {
